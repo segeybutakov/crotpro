fdf67183326050749117da88760599dedc7ebb67
dontk1 <tosin.komolafe@aun.edu.ng>
2012-03-25 19:42:22 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -73,10 +73,6 @@
                 clean_data();
             }
             
-            if($field == 'createaccount' && $value == true) {
-                setup_crotpro_free_account();
-            } 
-            
             if($field == 'testglobal' && $value == true) {
                 test_global_search();
             }
@@ -99,52 +95,6 @@ function clean_data(){
     $DB->delete_records("plagiarism_crotpro_job");
     $DB->delete_records("plagiarism_crotpro_files");
     notify(get_string('tables_cleaned_up','plagiarism_crotpro'), 'notifysuccess');
-}
-
-
-function setup_crotpro_free_account(){
-    global $CFG;
-    global $DB;
-    require_once($CFG->dirroot. '/plagiarism/crotpro/post_xml.php');
-    $plagiarismsettings = (array)get_config('plagiarism');
-    $service_url = $plagiarismsettings['crotpro_service_url']; // pds service link
-    $params = array();
-    $port = 80;
-    $url = $service_url.'/create_account.php';
-    echo "Setting up CrotPro free account<br><hr>";
-    $response = xml_post($params, $url, $port); // sends file to the web service
-    $xml = new DOMDocument();
-    if($xml->loadXML($response)){ // get back response
-       $m = $xml->getElementsByTagName("message");
-       $message = '';
-       if($m->length <> 0){
-           foreach($m as $value){
-               $message = $value->nodeValue;
-           }
-       }
-       
-       if($message == 'OK'){
-            $t = $xml->getElementsByTagName("uid");
-            $uid = '';
-            if($t->length <> 0){
-                foreach($t as $value){
-                    $uid = $value->nodeValue;
-                }
-            }
-           if($uid != ''){
-               if ($configfield = $DB->get_record('config_plugins', array('name'=>'crotpro_account_id', 'plugin'=>'plagiarism'))) {
-                    $configfield->value = $uid;
-                    if (! $DB->update_record('config_plugins', $configfield)) {
-                        error("errorupdating");
-                    }               
-               }
-            }  
-       }
-       echo "<i>- <font color=\"green\"><b>OK</b></font></i><hr>";
-       echo "Message from server: <i>".$message."</i><br>";
-
-    }
-    flush();
 }
 
 function test_global_search(){
